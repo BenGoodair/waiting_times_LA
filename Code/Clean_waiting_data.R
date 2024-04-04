90ec15445d01e12ea7f2e4e8f916d7695c041ac7
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2024-04-04 16:07:49 +0100

    my soul ascended to the sky

--- Code/Clean_waiting_data.R
+++ Code/Clean_waiting_data.R
@@ -8,22 +8,126 @@ file_vector <- list.files(path = "C:/Users/benjamin.goodair/OneDrive - Nexus365/
 #file_vector %>% head()
 csv_list <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/waiting_times_LA/main/Data/Raw_Data/2010/csvs/xl_list.csv"))[2]
 
-df <- data.frame()
+df_incomplete <- data.frame()
 
-for (i in c(1,4,7,10,13,16)) {
+for (i in c(1,3,4,15,18)) {
   waits <- read.csv(paste("C:/Users/benjamin.goodair/OneDrive - Nexus365/Documents/GitHub/waiting_times_LA/Data/Raw_Data/2010/csvs/",csv_list$x[i],".csv", sep=""))
-  if (nrow(df) == 0) {
-    df <- waits
+  if (i== 1) {
+    df_incomplete <- waits
   } else {
-    df <- rbind(df, waits)
+    df_incomplete <- rbind(df_incomplete, waits)
+  }
+  paste(i)
+}
+waits <- read.csv(paste("C:/Users/benjamin.goodair/OneDrive - Nexus365/Documents/GitHub/waiting_times_LA/Data/Raw_Data/2010/csvs/",csv_list$x[10],".csv", sep=""))
+waits <- waits %>% dplyr::mutate(Total.within.18.weeks = (X.17.18+X.11.12+X.5.6+
+                                                   X.16.17+X.10.11+X.0.1+
+                                                   X.15.16+X.9.10+X.4.5+
+                                                   X.14.15+X.8.9+X.3.4+
+                                                   X.13.14+X.7.8+X.2.3+
+                                                   X.12.13+X.6.7+X.1.2),
+                                 X..within.18.weeks = Total.within.18.weeks/Total.number.of.incomplete.pathways)
+
+df_incomplete <- rbind(df_incomplete, waits)
+
+
+
+
+
+df_admitted <- data.frame()
+
+for (i in c(5,8,9,11,14)) {
+  waits <- read.csv(paste("C:/Users/benjamin.goodair/OneDrive - Nexus365/Documents/GitHub/waiting_times_LA/Data/Raw_Data/2010/csvs/", csv_list$x[i],".csv", sep=""))
+  if (nrow(df_admitted) == 0) {
+    df_admitted <- waits
+  } else {
+    df_admitted <- rbind(df_admitted, waits)
   }
 }
 
-for (i in c(1, 4, 7, 10, 13, 16)) {
-  waits <- read.csv(paste("C:/Users/benjamin.goodair/OneDrive - Nexus365/Documents/GitHub/waiting_times_LA/Data/Raw_Data/2015/", csv_list$x[i], sep=""))
-  if (nrow(df) == 0) {
-    df <- waits
+waits <- read.csv(paste("C:/Users/benjamin.goodair/OneDrive - Nexus365/Documents/GitHub/waiting_times_LA/Data/Raw_Data/2010/csvs/",csv_list$x[17],".csv", sep=""))
+waits <- waits %>% dplyr::mutate(Total..with.a.known.clock.start..within.18.weeks = (X.17.18+X.11.12+X.5.6+
+                                                            X.16.17+X.10.11+X.0.1+
+                                                            X.15.16+X.9.10+X.4.5+
+                                                            X.14.15+X.8.9+X.3.4+
+                                                            X.13.14+X.7.8+X.2.3+
+                                                            X.12.13+X.6.7+X.1.2),
+                                 X..within.18.weeks = Total..with.a.known.clock.start..within.18.weeks/Total.number.of.completed.pathways..with.a.known.clock.start.)
+
+df_admitted <- rbind(df_admitted, waits)
+
+
+
+
+
+df_non_admitted <- data.frame()
+
+for (i in c(2,6,7,12,13)) {
+  waits <- read.csv(paste("C:/Users/benjamin.goodair/OneDrive - Nexus365/Documents/GitHub/waiting_times_LA/Data/Raw_Data/2010/csvs/", csv_list$x[i],".csv", sep=""))
+  if (nrow(df_non_admitted) == 0) {
+    df_non_admitted <- waits
   } else {
-    df <- rbind(df, waits)
+    df_non_admitted <- rbind(df_non_admitted, waits)
   }
 }
+
+waits <- read.csv(paste("C:/Users/benjamin.goodair/OneDrive - Nexus365/Documents/GitHub/waiting_times_LA/Data/Raw_Data/2010/csvs/",csv_list$x[16],".csv", sep=""))
+waits <- waits %>% dplyr::mutate(Total..with.a.known.clock.start..within.18.weeks = (X.17.18+X.11.12+X.5.6+
+                                                                                       X.16.17+X.10.11+X.0.1+
+                                                                                       X.15.16+X.9.10+X.4.5+
+                                                                                       X.14.15+X.8.9+X.3.4+
+                                                                                       X.13.14+X.7.8+X.2.3+
+                                                                                       X.12.13+X.6.7+X.1.2),
+                                 X..within.18.weeks = Total..with.a.known.clock.start..within.18.weeks/Total.number.of.completed.pathways..with.a.known.clock.start.)
+
+df_non_admitted <- rbind(df_non_admitted, waits)
+
+df_admitted <- df_admitted %>%dplyr::filter(Treatment.Function=="Total")%>%
+  dplyr::select(PCT.Code,PCT.Name,
+                Total.number.of.completed.pathways..with.a.known.clock.start.,
+                Total..with.a.known.clock.start..within.18.weeks,X..within.18.weeks,
+                X..within.18.weeks)%>%
+  dplyr::group_by(PCT.Code,PCT.Name)%>%
+  dplyr::summarise(   Total.number.of.completed.pathways..with.a.known.clock.start.= mean(Total.number.of.completed.pathways..with.a.known.clock.start.),
+                   Total..with.a.known.clock.start..within.18.weeks = mean(Total..with.a.known.clock.start..within.18.weeks),
+                   X..within.18.weeks = mean(X..within.18.weeks))%>%
+  dplyr::ungroup()%>%
+  dplyr::mutate(type="admitted", year=2010)%>%
+  dplyr::rename(Total_pathways = Total.number.of.completed.pathways..with.a.known.clock.start.,
+                Total_pathways_within_18_weeks = Total..with.a.known.clock.start..within.18.weeks)
+
+df_non_admitted <- df_non_admitted %>%dplyr::filter(Treatment.Function=="Total")%>%
+  dplyr::select(PCT.Code,PCT.Name,
+                Total.number.of.completed.pathways..with.a.known.clock.start.,
+                Total..with.a.known.clock.start..within.18.weeks,X..within.18.weeks,
+                X..within.18.weeks)%>%
+  dplyr::group_by(PCT.Code,PCT.Name)%>%
+  dplyr::summarise(Total.number.of.completed.pathways..with.a.known.clock.start.= mean(Total.number.of.completed.pathways..with.a.known.clock.start.),
+                   Total..with.a.known.clock.start..within.18.weeks = mean(Total..with.a.known.clock.start..within.18.weeks),
+                   X..within.18.weeks = mean(X..within.18.weeks))%>%
+  dplyr::ungroup()%>%
+  dplyr::mutate(type="non_admitted",
+                year=2010)%>%
+  dplyr::rename(Total_pathways = Total.number.of.completed.pathways..with.a.known.clock.start.,
+                Total_pathways_within_18_weeks = Total..with.a.known.clock.start..within.18.weeks)
+
+
+df_incomplete <- df_incomplete %>%dplyr::filter(Treatment.Function=="Total")%>%
+  dplyr::select(PCT.Code,PCT.Name,
+                Total.number.of.incomplete.pathways,
+                Total.within.18.weeks,
+                X..within.18.weeks)%>%
+  dplyr::group_by(PCT.Code,PCT.Name)%>%
+  dplyr::summarise(Total.number.of.incomplete.pathways = mean(Total.number.of.incomplete.pathways),
+                   Total.within.18.weeks= mean(Total.within.18.weeks),
+                   X..within.18.weeks = mean(X..within.18.weeks))%>%
+  dplyr::ungroup()%>%
+  dplyr::mutate(type="incomplete",
+                year=2010)%>%
+  dplyr::rename(Total_pathways = Total.number.of.incomplete.pathways,
+                Total_pathways_within_18_weeks = Total.within.18.weeks)
+
+df_2010 <- rbind(df_incomplete, df_admitted, df_non_admitted)
+
+
+
